a869dd77d307ded438f67a1f3c17a97f9a87f399
guosheng <gpromote@163.com>
2025-10-25 20:42:25 +0800

    Add JsonPropertyName attributes to CustomerRank properties for JSON serialization

--- Models/CustomerRank.cs
+++ Models/CustomerRank.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace ScoreRank.Models
 {
     /// <summary>
@@ -8,16 +10,19 @@ namespace ScoreRank.Models
         /// <summary>
         /// The unique identifier of the customer.
         /// </summary>
+        [JsonPropertyName("customer_id")]
         public long CustomerId { get; set; }
 
         /// <summary>
         /// The current score of the customer.
         /// </summary>
+        [JsonPropertyName("score")]
         public decimal Score { get; set; }
 
         /// <summary>
         /// The rank position of the customer in the leaderboard.
         /// </summary>
+        [JsonPropertyName("rank")]
         public int Rank { get; set; }
     }
 }

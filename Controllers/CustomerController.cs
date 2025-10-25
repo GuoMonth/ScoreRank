a3a3a75ee3c752b38138ccccd41e4f55e64eb97f
guosheng <gpromote@163.com>
2025-10-25 20:14:05 +0800

    Add UpdateScore method to CustomerController for score management

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -20,6 +20,19 @@ namespace ScoreRank.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Updates the score for a customer.
+        /// Note, If the customer does not exist, they are created with initial score of 0 before applying the update.
+        /// </summary>
+        /// <param name="customerId">The customer ID (positive int64).</param>
+        /// <param name="score">The score adjustment (decimal in range [-1000, +1000]). Positive increases, negative decreases.</param>
+        /// <returns>The current score after update.</returns>
+        [HttpPost("{customerId}/score/{score}")]
+        public ActionResult<decimal> UpdateScore(long customerId, decimal score)
+        {
+            // Implementation not required for this task
+            return Ok(0m);
+        }
     }
 }
 

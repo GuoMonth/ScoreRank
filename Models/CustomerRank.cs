ab1cdb62015003fa5e44fd90a93bb40d2e999fb2
guosheng <gpromote@163.com>
2025-10-26 19:26:52 +0800

    Change Rank property to nullable long and add custom comparer for sorting CustomerRank objects

--- Models/CustomerRank.cs
+++ Models/CustomerRank.cs
@@ -26,6 +26,33 @@ namespace ScoreRank.Models
         /// Note: Rank is always a positive integer. and never same rank for different customers.
         /// </summary>
         [JsonPropertyName("rank")]
-        public long Rank { get; set; }
+        public long? Rank { get; set; }
+    }
+
+    /// <summary>
+    /// Custom comparer to sort by Score descending, then by CustomerId ascending.
+    /// </summary>
+    public class Comparer : IComparer<CustomerRank>
+    {
+        /// <summary>
+        /// Compares two CustomerRank objects.
+        /// </summary>
+        /// <param name="x">The first CustomerRank object to compare.</param>
+        /// <param name="y">The second CustomerRank object to compare.</param>
+        /// <returns>A negative integer, zero, or a positive integer as the first argument is less than, equal to, or greater than the second.</returns>
+        public int Compare(CustomerRank? x, CustomerRank? y)
+        {
+            // Compare by Score descending
+            int result = y?.Score.CompareTo(x?.Score) ?? 0;
+            if (result != 0)
+            {
+                return result;
+            }
+            else
+            {
+                // If Scores are equal, compare by CustomerId ascending
+                return x?.CustomerId.CompareTo(y?.CustomerId) ?? 0;
+            }
+        }
     }
 }
